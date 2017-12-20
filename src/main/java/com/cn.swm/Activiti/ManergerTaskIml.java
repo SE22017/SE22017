c8e271ecebf3ff27b3ea07a9f61f0d6e321953cd
caozhengbin <141250006@smail.nju.edu.cn>
2017-12-20 15:28:51 +0800

    系统框架

---
+++ src/main/java/com/cn.swm/Activiti/ManergerTaskIml.java
@@ -0,0 +1,15 @@
+package com.cn.swm.Activiti;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+@SuppressWarnings("serial")
+public class ManergerTaskIml implements TaskListener {
+
+	@Override
+	public void notify(DelegateTask arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
